51bdfa06038fb0aee4aea14c508a7f42a55e91c6
Joe M <45021583+4d30@users.noreply.github.com>
2026-05-12 07:35:54 -0400

    remove rfc8785 submodule

--- rfc8785/__init__.py
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env python
-
-from ._jcs import canonical_bytes
--- rfc8785/_jcs.py
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env python
-
-from vendor.json_canonicalization.python3.src.org.webpki.json import Canonicalize
-
-def canonical_bytes(obj: dict) -> bytes:
-    return Canonicalize.canonicalize(obj)
